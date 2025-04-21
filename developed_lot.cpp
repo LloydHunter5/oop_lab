501842ccd8580db719c99b7add8a9c22af116bc4
Noah <nonochrisman@gmail.com>
2025-04-21 14:27:29 -0700

    probably doesn't work but class is almost over

--- developed_lot.cpp
+++ developed_lot.cpp
@@ -13,3 +13,12 @@ double developed_lot::DevelopedLot::floor_area_ratio(){
 double developed_lot::DevelopedLot::value(){
     return lot::Lot::value() + floor_area * floors * 100;
 }
+
+office::Office::Office(double lat, double lon, double w, double h, double a, int num_floors, int num_offices) : developed_lot::DevelopedLot::DevelopedLot(lat, lon, w, h, a, num_floors){
+	floor_area = a;
+	floors = num_floors;
+	offices = num_offices;
+}
+
+double office::Office::value(){
+	return developedlot::DevelopedLot::value() + floor_area * floors + num_offices * 200;
