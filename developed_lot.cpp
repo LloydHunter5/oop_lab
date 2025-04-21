c0f274ec5b27ab4957965d4d7aa65669a48be26e
Noah <nonochrisman@gmail.com>
2025-04-21 14:30:02 -0700

    probably doesn't work but class is almost over

--- developed_lot.cpp
+++ developed_lot.cpp
@@ -15,8 +15,6 @@ double developed_lot::DevelopedLot::value(){
 }
 
 office::Office::Office(double lat, double lon, double w, double h, double a, int num_floors, int num_offices) : developed_lot::DevelopedLot::DevelopedLot(lat, lon, w, h, a, num_floors){
-	floor_area = a;
-	floors = num_floors;
 	offices = num_offices;
 }
 
